b597cee35217905e1809ca14691f6654718d7e49
Andres <martinezmiguel.0098@gmail.com>
2024-10-01 10:28:10 -0500

    :hammer: new settings

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-function search_Id() {
-    let search = document.getElementById("btn-primary").value.trim();
+function search_ID() {
+    let search = document.getElementById("Search").value.trim();
     if (search === "") {
+        console.log(search);
         return;
     }
+
     let URL;
+
     if (!isNaN(search)) {
-        URL = `hhttps://imdb.iamidiotareyoutoo.com/justwatch?q=/${search}`;
+        URL = `https://imdb.iamidiotareyoutoo.com/justwatch?q=${search}`;
     } else {
         URL = `https://imdb.iamidiotareyoutoo.com/justwatch?q=${search}`;
     }
 
     fetch(URL)
-        .then(Response => Response.json())
+        .then(res => res.json())
         .then(data => {
 
             if (Array.isArray(data)) {
                 if (data.length === 0) {
-                    document.getElementById("view").innerHTML = `<p>No se encontraron peliculas</p>`;
+                    document.getElementById("view").innerHTML = `<p>No se encontraron usuarios</p>`;
                     return;
                 }
 
-                data.forEach(data => {
+                let UsersHTML = '<ul class="list-group">';
+
+                data.forEach(user => {
                     UsersHTML += `
-                    <div class="blue">    
-                        <h2>${data.description.title}</h2>
-                        <img id="characterImage" src="${data.description.photo_url[0]}" class="img-thumbnail mb-3" alt="${data.description.photo_url[0]}">
-                    <table class="table table-striped">
-                        <p>Nombre: ${data.description.title}</p>
-                        <p>ID: ${data.description.id}</p>
-                        <p>Year: ${data.description.year}</p>
-                    </table>
-                </div>
-                `;
+                    <div class="blue2">
+                        <img id="characterImage" src="${user.avatar}" class="img-thumbnail mb-3" alt="${user.name_full}">
+                        <table class="table table-striped">
+                            <tr class="Name"><th scope="col">Nombre: </th><td>${user.name_full}</td></tr>
+                            <tr class="Descripcion"><th scope="col">Descripción: </th><td>${user.description}</td></tr>
+                            <tr class="ID"><th scope="col">ID: </th><td>${user.id}</td></tr>
+                        </table>
+                    </div>
+                    `;
                 });
 
                 UsersHTML += '</ul>';
@@ -45,12 +49,12 @@ function search_Id() {
                 }
                 let UsersHTML = `
                 <div class="blue">    
-                        <h2>${data.description.title}</h2>
-                        <img id="characterImage" src="${data.description.photo_url[0]}" class="img-thumbnail mb-3" alt="${data.description.photo_url[0]}">
+                        <h2>${data.name_full}</h2>
+                        <img id="characterImage" src="${data.avatar}" class="img-thumbnail mb-3" alt="${data.name_full}">
                     <table class="table table-striped">
-                        <p>Nombre:${data.description.title}</p>
-                        <p>ID:${data.description.id}</p>
-                        <p>Year:${data.description.year}</p>
+                        <tr><th scope="col">Nombre: </th><td>${data.name_full}</td></tr>
+                        <tr><th scope="col">Descripción: </th><td>${data.description}</td></tr>
+                        <tr><th scope="col">ID: </th><td>${data.id}</td></tr>
                     </table>
                 </div>
                 `;
@@ -58,8 +62,11 @@ function search_Id() {
             }
         })
         .catch(error => {
-            document.getElementById("view").innerHTML = `<p>Error al buscar la pelicula </p>`;
-            console.error('Error al buscar la pelicula: ', error);
+            document.getElementById("view").innerHTML = `<p>Error al buscar usuario</p>`;
+            console.error('Error al buscar usuario:', error);
         });
 }
-//URL ID or Name: https://imdb.iamidiotareyoutoo.com/justwatch?q=12
+
+document.getElementById("Search").addEventListener("input", function () {
+    search_ID();
+});
